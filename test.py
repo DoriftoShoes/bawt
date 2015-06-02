3f392754bfcfabdd0040f6120c93e19f268ab41a
Patrick Hoolboom <self@dorifto.shoes>
2015-06-02 22:47:22 +0000

    Modified test to support irrigation subsystem and timd_run

--- test.py
+++ test.py
@@ -1,33 +1,19 @@
 from switchboard.pin import Pin
 from switchboard.board import Board
-from bawt.base import Base
+from subsystems.irrigation import Irrigation
 
-import time
 import sys
-
-class Irrigation(Base):
-
-    def get_zone(self, zone):
-        return self.config['zones'][zone]
-        
-    def start(self, zone):
-        zone_definition = self.get_zone(zone)
-        pin = Pin(zone_definition['pin'])
-        pin.on()
-        
-    def stop(self, zone):
-        zone_definition = self.get_zone(zone)
-        pin = Pin(zone_definition['pin'])
-        pin.off()
-
 
 if __name__ == "__main__":
     zone = int(sys.argv[1])
     state = sys.argv[2]
+    run_time = int(sys.argv[3])
 
     irrigation = Irrigation()
 
     if state == 'on':
         irrigation.start(zone)
     elif state == 'off':
-        irrigation.stop(zone)   
+        irrigation.stop(zone)
+    elif state == 'timed':
+        irrigation.timed_run(zone, run_time)   
