eb1727bf37d888bb2a68babf7bfb90b8c004cdb7
Patrick Hoolboom <self@dorifto.shoes>
2015-06-17 10:11:52 -0700

    Moved some methods around

--- subsystems/irrigation.py
+++ subsystems/irrigation.py
@@ -11,15 +11,20 @@ class Irrigation(Base):
 
     def get_zone(self, zone):
         return self.irrigation['zones'][zone]
-        
+
+    def _get_zone_definition(self, zone):
+        return self.get_zone(zone)
+
+    def _get_zone_pin(self, zone):
+        pin_number = self._get_zone_definition(zone)
+        return Pin(pin_number)
+
     def start(self, zone):
-        zone_definition = self.get_zone(zone)
-        pin = Pin(zone_definition['pin'])
+        pin = self._get_zone_pin(zone)
         pin.on()
         
     def stop(self, zone):
-        zone_definition = self.get_zone(zone)
-        pin = Pin(zone_definition['pin'])
+        pin = self._get_zone_pin(zone)
         pin.off()
 
     def timed_run(self, zone, run_time):
