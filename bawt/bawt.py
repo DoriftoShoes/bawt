3f1d03f502df89cf42743048d93cf65bfd0f036f
DoriftoShoes <self@dorifto.shoes>
2015-12-01 05:02:46 +0000

    Added option to specify path for specific config files

--- bawt/bawt.py
+++ bawt/bawt.py
@@ -14,8 +14,12 @@ class Bawt(object):
         for subsystem, config in self.subsystems.iteritems():
             if config.get('enabled', False):
                 try:
-                    conf_file = "conf/%s.yaml" % subsystem
-                    conf = yaml.safe_load(open(conf_file))
+                    conf_file = self.config.get('config', None)
+                    if conf_file:
+                        conf_file_path = conf_file
+                    else:
+                        conf_file_path = "conf/%s.yaml" % subsystem
+                    conf = yaml.safe_load(open(conf_file_path))
                     setattr(self, subsystem, conf)
                 except Exception as e:
                     print("Could not activate %s subsystem.  Error: %s" % (subsystem, str(e)))
