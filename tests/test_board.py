6b058d9c920e311c3b8e0fb4c5a7a2a5b9aeee81
DoriftoShoes <self@dorifto.shoes>
2015-12-12 03:13:26 +0000

    First attempt at mocking

--- tests/test_board.py
+++ tests/test_board.py
@@ -1,20 +1,17 @@
+import mock
 import unittest
 
-try:
-    import RPi.GPIO as GPIO
-    print GPIO.BCM
-except:
-    from bawt.mock.RPi import GPIO
-    print(GPIO.ANNOUNCEMENT)
+import RPi.GPIO as GPIO
 
 from bawt.switchboard.board import Board
 from bawt.switchboard.pin import Pin
 
+@mock.patch("RPi.GPIO.setmode", autospec=True)
 class TestBoard(unittest.TestCase):
 
     BOARD = Board()
 
-    def test_board_set_mode(self):
+    def test_board_set_mode(self, mock_mode):
         TestBoard.BOARD.set_mode(GPIO.BCM)
         self.assertEquals(TestBoard.BOARD.mode,GPIO.BCM, 'Board mode not set correctly')
 
