5b9a01c0504c2cdf3e4291209defbde229915345
DoriftoShoes <self@dorifto.shoes>
2015-12-12 01:36:13 +0000

    Adding board test

---
+++ tests/test_board.py
@@ -0,0 +1,16 @@
+import unittest
+
+import RPi.GPIO as GPIO
+from bawt.switchboard.board import Board
+from bawt.switchboard.pin import Pin
+
+class TestBoard(unittest.TestCase):
+
+    BOARD = Board()
+
+    def test_board_set_mode(self):
+        TestBoard.BOARD.set_mode(GPIO.BCM)
+        self.assertEquals(TestBoard.BOARD.mode,GPIO.BCM, 'Board mode not set correctly')
+
+if __name__ == '__main__':
+    unittest.main()
