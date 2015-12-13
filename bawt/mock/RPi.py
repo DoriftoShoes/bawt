07054a3248ff108ed511ead045c1770e7d445eaa
Patrick Hoolboom <patrick@stackstorm.com>
2015-12-13 12:49:34 -0800

    Adding some statics

--- bawt/mock/RPi.py
+++ bawt/mock/RPi.py
@@ -1,9 +1,26 @@
 
 class GPIO(object):
 
-    BCM = 1
-    OUT = 1
+    BCM = 11
+    BOARD = 10
+    BOTH = 33
+    FALLING = 32
+    HARD_PWM = 43
+    HIGH = 1
+    I2C = 42
     IN = 1
+    LOW = 0
+    OUT = 0
+    PUD_DOWN = 21
+    PUD_OFF = 20
+    PUD_UP = 22
+    RISING = 31
+    RPI_INFO = {'P1_REVISION': 3, 'RAM': '1024M', 'REVISION': 'a21041', 'TYPE': 'Pi2 Model B', 'PROCESSOR': 'BCM2836', 'MANUFACTURER': 'Embest'}
+    RPI_REVISION = 3
+    SERIAL = 40
+    SPI = 41
+    UNKNOWN = -1
+    VERSION = '0.5.11'
     ANNOUNCEMENT = '''
     ############################################################
     #                                                          #
