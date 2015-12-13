a4d9fb8e4c8e75164f6c983acf4925f4ddc740e4
Patrick Hoolboom <patrick@stackstorm.com>
2015-12-13 15:08:11 -0800

    Adding hour range support.

--- bawt/bin/timelapsed.py
+++ bawt/bin/timelapsed.py
@@ -1,6 +1,7 @@
 import argparse
 import sys
 import time
+from datetime import datetime
 
 import bawt.log as logging
 from bawt.subsystems.camera import Camera
@@ -12,18 +13,27 @@ class Timelapsed(Camera):
 
     def __init__(self, argv):
         self.args = None
+        self.frequency = None
+        self.prefix = None
+        self.delete = None
+        self.hours = 'all'
         self.parse_args(argv)
 
         super(Timelapsed, self).__init__(config_dir=self.args.config_dir)
 
         LOG.info("Starting Timelapsed")
         self.setup()
+
+    def setup(self):
+        super(Timelapsed, self).setup()
         self.frequency = self.args.frequency if self.args.frequency else self.timelapse.get('frequency', 60)
         self.prefix = self.args.prefix if self.args.prefix else self.timelapse.get('prefix', None)
         self.delete = self.args.delete if self.args.delete else self.timelapse.get('delete', False)
+        self.hours = self.args.hours if self.args.hours else self.timelapse.get('hours', 'all')
         LOG.info("Frequency set to %s seconds" % self.frequency)
         LOG.info("File prefix set to %s" % self.prefix)
         LOG.info("Local file delete is set to %s" % self.delete)
+        LOG.info("Hours set to %s" % self.hours)
 
     def parse_args(self, argv):
 
@@ -41,6 +51,9 @@ class Timelapsed(Camera):
         parser.add_argument('--delete',
                             dest='delete',
                             help='Delete local file')
+        parser.add_argument('--hours',
+                            dest='hours',
+                            help='Hours to run timelapse')
 
         self.args = parser.parse_args(args=argv)
 
@@ -48,12 +61,26 @@ class Timelapsed(Camera):
         self.get_picture(name=self.prefix, use_timestamp=True)
         self.remote_save(delete_local=self.delete)
 
+    def check_time(self):
+        if self.hours == 'all':
+            return True
+
+        current_hour = datetime.now().hour
+        if current_hour in self.hours:
+            return True
+
+        return False
+
     def run(self):
         if self.timelapse.get('enabled', False):
             while True:
-                self.get_pic()
-                LOG.info("Sleeping for %s seconds..." % self.frequency)
-                time.sleep(float(self.frequency))
+                if self.check_time():
+                    self.get_pic()
+                    LOG.info("Sleeping for %s seconds..." % self.frequency)
+                    time.sleep(float(self.frequency))
+                else:
+                    LOG.info("Current hour %i is not in the list of enabled hours")
+                    time.sleep(300)
         else:
             LOG.info('Timelapse is disabled.  Please review your config file.')
 
