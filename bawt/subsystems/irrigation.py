feaecd9ed6ac0cbb9b67b0f01d11ea7c835323c7
DoriftoShoes <self@dorifto.shoes>
2016-02-18 09:37:25 -0800

    blah

--- bawt/subsystems/irrigation.py
+++ bawt/subsystems/irrigation.py
@@ -13,7 +13,7 @@ class Irrigation(Bawt):
         super(self.__class__, self).__init__()
 
     def get_zone(self, zone):
-        return self.irrigation['zones'][zone]
+        return self.irrigation['units'][zone]
 
     def _get_zone_definition(self, zone):
         return self.get_zone(zone)
@@ -32,14 +32,17 @@ class Irrigation(Bawt):
         return self.get_runs()[run].get('zones', None)
 
     def start(self, zone):
+        LOG.info("Starting zone %i" % zone)
         pin = self._get_zone_pin(zone)
         pin.on()
 
     def stop(self, zone):
+        LOG.info("Stopping zone %i" % zone)
         pin = self._get_zone_pin(zone)
         pin.off()
 
     def timed_run(self, zone, run_time):
+        LOG.info("Timed run of %is requested for zone %i" % (run_time, zone))
         thread = Thread(target=self.start(zone))
         thread.daemon = True
         thread.start()
